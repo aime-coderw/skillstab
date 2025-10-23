4e57cc85b656a2e4acfa37f6290920bcc52588de
aime-coderw <aimemeddy25@gmail.com>
2025-10-23 18:59:21 +0200

    Add supabase client

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { supabase } from "./supabaseClient";
+import { supabase } from "./supabaseClient.js";
 import "./Signup.css";
 
 export default function Signup() {
